5017fec787fdbefd3bca55700099b5491e0cf585
delficasarino <delficasarino@gmail.com>
2021-06-20 20:47:09 -0300

    actualización usando flexbox y medidas vw

--- src/routes/web.js
+++ src/routes/web.js
@@ -7,6 +7,7 @@ app.get("/", (req,res) => res.sendFile(path.resolve(__dirname, "../views/home.ht
 
 app.get("/register", (req,res) => res.sendFile(path.resolve(__dirname, "../views/register.html")));
 app.get("/login", (req,res) => res.sendFile(path.resolve(__dirname, "../views/login.html")));
+app.get("/formulario", (req,res) => res.sendFile(path.resolve(__dirname, "../views/formulario.html")));
 
 app.post("/", (req,res) => res.sendFile(path.resolve(__dirname, "../views/home.html")));
 
